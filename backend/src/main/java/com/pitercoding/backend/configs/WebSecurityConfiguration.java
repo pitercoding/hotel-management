f3e9fa5a2fa1a2d2f61b0317e0df4634f89ab649
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2026-03-20 15:16:49 -0300

    feat(security): configure Spring Security and implement custom CORS filter

---
+++ backend/src/main/java/com/pitercoding/backend/configs/WebSecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.pitercoding.backend.configs;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(request ->
+                request.requestMatchers("/api/auth/**").permitAll());
+
+        return http.build();
+    }
+}
