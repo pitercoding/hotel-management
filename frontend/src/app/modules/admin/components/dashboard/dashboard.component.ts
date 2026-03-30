8daac76e68bdeb75b260e3ae170d301ae6f383f0
pitercoding <piterg.bio@gmail.com>
2026-03-30 20:15:49 +0200

    feat(room): call get all rooms API in Angular with pagination, JWT authorization, and dashboard data binding

--- frontend/src/app/modules/admin/components/dashboard/dashboard.component.ts
+++ frontend/src/app/modules/admin/components/dashboard/dashboard.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {
+  AdminServices,
+  RoomDto,
+  RoomsResponse,
+} from '../../admin-services/admin.services';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,4 +12,31 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+
+  currentPage = 1;
+  totalPages = 0;
+  rooms: RoomDto[] = [];
+
+  constructor(
+    private adminService: AdminServices,
+    private message: NzMessageService
+  ) {
+    this.getRooms();
+  }
+
+  getRooms() {
+    this.adminService.getRooms(this.currentPage - 1).subscribe({
+      next: (res: RoomsResponse) => {
+        this.rooms = res.roomDtoList;
+        this.totalPages = res.totalPages;
+        this.currentPage = res.pageNumber + 1;
+      },
+      error: () => {
+        this.message.error('Unable to load rooms right now.', {
+          nzDuration: 5000,
+        });
+      },
+    });
+  }
+}
