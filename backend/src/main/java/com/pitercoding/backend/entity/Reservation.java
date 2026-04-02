088cda67f081700c4144483ca00578e9c575ed30
pitercoding <piterg.bio@gmail.com>
2026-04-02 10:55:46 +0200

    feat(reservation): implement reservation domain with entity, repository and status enum

---
+++ backend/src/main/java/com/pitercoding/backend/entity/Reservation.java
@@ -0,0 +1,33 @@
+package com.pitercoding.backend.entity;
+
+import com.pitercoding.backend.enums.ReservationStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private Long price;
+    private ReservationStatus reservationStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "room_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Room room;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
+}
