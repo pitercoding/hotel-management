0d072d3ff86db3bb5fa391b308bae15f9fb740f7
pitercoding <piterg.bio@gmail.com>
2026-04-04 10:30:06 +0200

    feat(booking): implement paginated bookings list for admin

--- frontend/src/app/modules/admin/admin-routing.module.ts
+++ frontend/src/app/modules/admin/admin-routing.module.ts
@@ -4,6 +4,7 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { PostRoomComponent } from './components/post-room/post-room.component';
 import { AdminComponent } from './admin.component';
 import { UpdateRoomComponent } from './components/updateroom/updateroom.component';
+import { ReservationsComponent } from './components/reservations/reservations.component';
 
 const routes: Routes = [
   {
@@ -15,6 +16,7 @@ const routes: Routes = [
       { path: 'rooms', component: DashboardComponent },
       { path: 'room', component: PostRoomComponent },
       { path: 'room/:id/edit', component: UpdateRoomComponent },
+      { path: 'reservations', component: ReservationsComponent },
     ],
   },
 ];
